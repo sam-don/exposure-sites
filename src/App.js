aa3a2cb2de1deb5b87db2e6379f115894c21eddf
Samitha <samitha.312+1@gmail.com>
2021-05-28 10:23:39 +1000

    made things a little bit prettier

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import icon from './Exposure-Site-Icon.svg'
 import MyComponent from './components/Map'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>VIC Covid-19 Exposure Sites</h1>
-        <MyComponent />
+        <img className="App-logo" src={icon} />
+        <h1>
+          {/* {icon} */}
+          VIC Covid-19 Exposure Sites
+        </h1>
       </header>
+      <main>
+        <MyComponent />
+      </main>
     </div>
   );
 }
