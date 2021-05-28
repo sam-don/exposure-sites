6279440c4d5f727ba870043d2167fbaa92c1bda7
Samitha <samitha.312+1@gmail.com>
2021-05-28 11:50:44 +1000

    added links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import icon from './Exposure-Site-Icon.svg'
 import MyComponent from './components/Map'
+import instaIcon from './assets/instagram-logo.svg'
+import linkedInIcon from './assets/linkedin.svg'
 
 function App() {
   return (
@@ -12,9 +14,16 @@ function App() {
           VIC Covid-19 Exposure Sites
         </h1>
       </header>
+      <div className="links">
+        <a href="https://www.coronavirus.vic.gov.au/exposure-sites">Find more info on the Vic Gov page</a>
+        <a href="https://www.covid19data.com.au/">More Covid-19 related data</a>
+      </div>
       <main>
         <MyComponent />
       </main>
+      <div className ="findMe">
+        Find me here: <a href="https://www.instagram.com/sam___don/"><img className="logo" src={instaIcon} /></a> <a href="https://www.linkedin.com/in/samitha-don-95518318a/"><img className="logo" src={linkedInIcon} /></a>
+      </div>
     </div>
   );
 }
